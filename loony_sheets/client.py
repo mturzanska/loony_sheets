86cf32553b946e43944f9a19ff33c2cca6bdc031
mturzanska <turzanska90@gmail.com>
2018-01-21 13:14:39 -0500

    Define Connection interface
    Define Cursor interface

--- loony_sheets/client.py
+++ loony_sheets/client.py
@@ -14,7 +14,62 @@ class Client:
         return cls(creds=creds)
 
     def __init__(self, creds):
-        self.connection = self._get_connection(creds)
+        self.connection = Connection(creds)
 
-    def _get_connection(self, creds):
-        return gspread.authorize(creds)
+
+class Connection:
+
+    def __init__(self, creds):
+        self._gsheets_client = gspread.authorize(creds)
+        self.cursor = self._cursor
+
+    def close(self):
+        self._gsheets_client = None
+        self._cursor = None
+
+    def commit(self):
+        pass
+
+    def _cursor(self):
+        pass
+
+
+class Cursor:
+
+    def __init__(self, gsheets_client):
+        self._gsheets_client = gsheets_client
+        self._description = None
+        self._rowcount = -1
+        self.arraysize = 1
+
+    @property
+    def description(self):
+        return self._description
+
+    @property
+    def rowcount(self):
+        return self._rowcount
+
+    def close(self):
+        self._gsheets_client = None
+
+    def execute(self):
+        pass
+
+    def executemany(self):
+        pass
+
+    def fetchone(self):
+        pass
+
+    def fetchmany(self):
+        pass
+
+    def fetchall(self):
+        pass
+
+    def setinputsizes(self):
+        pass
+
+    def setinputsize(self):
+        pass
